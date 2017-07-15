a272ca9c74fbefe0dd52471899adb6e54ad614b5
Nilesh Kesar <NKesar@fullsail.edu>
2017-07-14 23:47:39 -0400

    Finshed health

--- GMTK game jam july/Assets/Script/PlayerController.cs
+++ GMTK game jam july/Assets/Script/PlayerController.cs
@@ -6,20 +6,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+
     [SerializeField] GameObject player;
     KeyCode up    = KeyCode.W;
     KeyCode down  = KeyCode.S;
     KeyCode left  = KeyCode.A;
     KeyCode right = KeyCode.D;
 
+    int Maxhealth = 100;
+    int Currenthealth;
+
     // Use this for initialization
     void Start ()
     {
-		
+        Currenthealth = Maxhealth;
 	}
-	
-	// Update is called once per frame
-	void Update ()
+
+ // Update is called once per frame
+    void Update ()
     {
 		if(Input.GetKeyDown(up))
         {
@@ -36,6 +40,41 @@ public class PlayerController : MonoBehaviour {
         if (Input.GetKeyDown(right))
         {
             player.transform.Translate(1, 0, 0);
+           
         }
     }
+
+    //health system for the game    
+  #region Healthsystem
+
+    public int GetHealth()
+    {
+        return Currenthealth;
+    }
+
+    public void SetHealth(int _health)
+    {
+        if (_health + Currenthealth >= Maxhealth)
+        {
+            Currenthealth = Maxhealth;
+        }
+        else
+        {
+            Currenthealth += _health;
+        }
+    }
+    public void TakeDamage(int damage)
+    {
+        if (Currenthealth - damage <= 0)
+        {
+            Currenthealth = 0;
+        }
+        else
+        {
+            Currenthealth -= damage;
+        }
+    }
+
+#endregion
+
 }
