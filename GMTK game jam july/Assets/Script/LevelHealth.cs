b46916be879e1c322897b8b66698ea1282ac4079
Nilesh Kesar <NKesar@fullsail.edu>
2017-07-15 17:03:17 -0400

    YO LUKE FUCC U

---
+++ GMTK game jam july/Assets/Script/LevelHealth.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelHealth : MonoBehaviour {
+
+    int LevelMaxHealth = 100000; // 100,000 level max heatlh
+    int LevelCurrentHealth; //level current health;
+    public int Avgmaxhealth; //the percent of health left
+    int damage; // how much damage the blocc that was destoryed deals
+    Levelreposive flame; // used to get what blocc was destroyed
+
+    int innerlayer = 5; // top layer of level if blocc is destroyed 
+    int midlayer = 15; //  middle layer of level if blocc is destroyed
+    int outlayer = 30; //  outter layer of level if blocc is destroyed
+	// Use this for initialization
+	void Start () {
+        LevelCurrentHealth = LevelMaxHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       
+    }
+    public void updatehealth(int _damage)
+    {
+        damage = _damage;
+
+        if (damage == innerlayer)
+        {
+            LevelCurrentHealth -= damage;
+        }
+
+        else if (damage == midlayer)
+        {
+            LevelCurrentHealth -= damage;
+        }
+
+        else if (damage == outlayer)
+        {
+            LevelCurrentHealth -= damage;
+        }
+        Avgmaxhealth = LevelCurrentHealth / LevelMaxHealth;
+        if (Avgmaxhealth < 50)
+        {
+            SceneManager.LoadScene("");
+        }
+    }
+    public int GetAvgHealth()
+    {
+        return Avgmaxhealth = LevelCurrentHealth / LevelMaxHealth;
+    }
+}
---
+++ GMTK game jam july/Assets/Script/Levelreposive.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Levelreposive : MonoBehaviour {
+    int damage;
+    public LevelHealth levheal; // level's health
+
+    string innerlayer = "InnerLayer";
+    string midlayer = "MiddleLayer";
+    string outterlayer = "Outterlayer";
+
+    int damageinnerlayer = 5; // top layer of level if blocc is destroyed 
+    int damagemidlayer = 15; //  middle layer of level if blocc is destroyed
+    int damageoutlayer = 30; //  outter layer of level if blocc is destroyed
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "PlayerBullet")
+        {
+           if(tag == innerlayer)
+            {
+                damage = damageinnerlayer;
+                levheal.updatehealth(damage);
+            }
+
+           else if (tag == midlayer)
+            {
+                damage = damagemidlayer;
+                levheal.updatehealth(damage);
+            }
+
+            else if (tag == outterlayer)
+            {
+                damage = damageoutlayer;
+                levheal.updatehealth(damage);
+            }
+
+        }
+        
+    }
+}
