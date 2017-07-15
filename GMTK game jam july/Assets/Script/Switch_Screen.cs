183693e4b72033c376d4a021b2c709ed546fff66
Raj Chandan <RPChandan@fullsail.edu>
2017-07-15 01:40:27 -0400

    Menu part one done

---
+++ GMTK game jam july/Assets/Script/Switch_Screen.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Switch_Screen : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void switch_screen(string screen)
+    {
+        SceneManager.LoadScene(screen);
+    }
+}
